ab0de387c95acba7ccbfb9a1d1688f3c2c78922b
Monojoy Dey <140900504+monojoycodes@users.noreply.github.com>
2025-04-07 21:42:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,4 @@ def read_root():
     }
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
